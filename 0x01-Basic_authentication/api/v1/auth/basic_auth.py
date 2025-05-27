fd31a4acc2749f4ba0ae50c7db035134c4e63685
Hassan Ibrahim <hassan2002_kimo@hotmail.com>
2025-05-27 20:12:29 +0000

    0x01 task 11

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,6 +7,7 @@ import binascii
 from typing import Tuple, TypeVar
 from models.user import User
 
+
 class BasicAuth(Auth):
     """Basic auth class"""
     def extract_base64_authorization_header(
@@ -77,3 +78,12 @@ class BasicAuth(Auth):
                 return users[0]
         return None
 
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        retrieves the User instance for a request
+        """
+        auth_header = self.authorization_header(request)
+        b64_auth_token = self.extract_base64_authorization_header(auth_header)
+        auth_token = self.decode_base64_authorization_header(b64_auth_token)
+        email, password = self.extract_user_credentials(auth_token)
+        return self.user_object_from_credentials(email, password)
