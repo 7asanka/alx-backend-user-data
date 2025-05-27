5c8716234328d145596cdabb476a108c073ccad3
Hassan Ibrahim <hassan2002_kimo@hotmail.com>
2025-05-27 18:34:43 +0000

    0x01 task 7 added

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python3
 """ Basic auth module """
 from .auth import Auth
-
+import re
 
 class BasicAuth(Auth):
     """Basic auth class"""
-    pass
+    def extract_base64_authorization_header(
+            self,
+            authorization_header: str) -> str:
+        """
+        returns the Base64 part of the Authorization
+        header for a Basic Authentication
+        """
+        if type(authorization_header) == str:
+            pattern = r'Basic (?P<token>.+)'
+            field_match = re.fullmatch(pattern, authorization_header.strip())
+            if field_match is not None:
+                return field_match.group('token')
+        return None
