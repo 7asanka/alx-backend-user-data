ad64955754a336affbfc97b63ca079fa2fde3022
Hassan Ibrahim <hassan2002_kimo@hotmail.com>
2025-05-29 13:48:55 +0000

    0x02 task 3 fix

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -11,7 +11,7 @@ class SessionAuth(Auth):
     def create_session(self, user_id: str = None) -> str:
         """create a Session ID for a user_id"""
         if type(user_id) is str:
-            session_id = uuid4()
+            session_id = str(uuid4())
             self.user_id_by_session_id[session_id] = user_id
             return session_id
         return None
@@ -20,4 +20,3 @@ class SessionAuth(Auth):
         """returns a User ID based on a Session ID"""
         if type(session_id) is str:
             return self.user_id_by_session_id.get(session_id)
-        return None
