4df9882e9ca2d26c0c956d25a5f021cd2bd8e0ca
Hassan Ibrahim <hassan2002_kimo@hotmail.com>
2025-05-29 13:59:21 +0000

    0x02 task 4

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -2,6 +2,7 @@
 """ API authentication class module """
 
 
+import os
 from flask import request
 from typing import List, TypeVar
 
@@ -33,3 +34,10 @@ class Auth():
     def current_user(self, request=None) -> TypeVar('User'):
         """ implement later """
         return None
+
+    def session_cookie(self, request=None):
+        """returns a cookie value from a request"""
+        if request is None:
+            return None
+        cookie_name = os.getenv('SESSION_NAME')
+        return request.cookies.get(cookie_name)
