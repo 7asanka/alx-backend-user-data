b59d2f47cf970b2902db99d0b57cb6a0937f391c
Hassan Ibrahim <hassan2002_kimo@hotmail.com>
2025-06-03 20:16:23 +0000

    0x03 task 10

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -47,3 +47,15 @@ class Auth:
         except NoResultFound:
             return False
         return False
+
+    def create_session(self, email: str) -> str:
+        """ creates a new session """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return None
+        if user is None:
+            return None
+        session_id = _generate_uuid()
+        self._db.update_user(user.id, session_id=session_id)
+        return session_id
