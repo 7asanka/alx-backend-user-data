61e4513551f047a49ed5513c122332373c459f6e
Hassan Ibrahim <hassan2002_kimo@hotmail.com>
2025-06-03 13:52:05 +0000

    0x03 task 1-2

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -46,16 +46,12 @@ class DB:
 
     def find_user_by(self, **kwargs) -> User:
         """Return a user based on filtered keyworded arguments"""
-        fields, values = [], []
-        for key, value in kwargs.items():
-            if hasattr(User, key):
-                fields.append(getattr(User, key))
-                values.append(value)
-            else:
-                raise InvalidRequestError()
-        result = self._session.query(User).filter(
-            tuple_(*fields).in_([tuple(values)])
-        ).first()
-        if result is None:
-            raise NoResultFound()
-        return result
+        if not kwargs:
+            raise InvalidRequestError("No filter arguments provided")
+        try:
+            user = self._session.query(User).filter_by(**kwargs).one()
+        except NoResultFound:
+            raise
+        except InvalidRequestError:
+            raise
+        return user
