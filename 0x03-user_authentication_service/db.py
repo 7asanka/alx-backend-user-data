44f48a7adc5fdb63a9c601112840112b63fd85a5
Hassan Ibrahim <hassan2002_kimo@hotmail.com>
2025-06-03 13:57:51 +0000

    0x03 tasks 1-2

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 """DB module
 """
-from sqlalchemy import create_engine, tuple_
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.session import Session
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.orm.session import Session
@@ -41,7 +40,7 @@ class DB:
             self._session.commit()
         except Exception:
             self._session.rollback()
-            new_user = None
+            raise
         return new_user
 
     def find_user_by(self, **kwargs) -> User:
