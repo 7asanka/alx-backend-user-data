faeb924664c90a4fbcb99d209eb72d0a5577d9dc
Hassan Ibrahim <hassan2002_kimo@hotmail.com>
2025-06-03 14:03:33 +0000

    0x03 test

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """DB module
 """
+
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
@@ -12,12 +12,10 @@ from user import Base, User
 
 
 class DB:
-    """DB class
-    """
+    """DB class"""
 
     def __init__(self) -> None:
-        """Initialize a new DB instance
-        """
+        """Initialize a new DB instance"""
         self._engine = create_engine("sqlite:///a.db", echo=True)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
@@ -25,32 +23,24 @@ class DB:
 
     @property
     def _session(self) -> Session:
-        """Memoized session object
-        """
+        """Memoized session object"""
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """adds a user to the database"""
-        try:
-            new_user = User(email=email, hashed_password=hashed_password)
-            self._session.add(new_user)
-            self._session.commit()
-        except Exception:
-            self._session.rollback()
-            raise
+        """Adds a user to the database"""
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
+        self._session.commit()
         return new_user
 
     def find_user_by(self, **kwargs) -> User:
-        """Return a user based on filtered keyworded arguments"""
+        """Find a user by arbitrary keyword filters"""
         if not kwargs:
             raise InvalidRequestError("No filter arguments provided")
         try:
-            user = self._session.query(User).filter_by(**kwargs).one()
-        except NoResultFound:
+            return self._session.query(User).filter_by(**kwargs).one()
+        except (NoResultFound, InvalidRequestError):
             raise
-        except InvalidRequestError:
-            raise
-        return user
