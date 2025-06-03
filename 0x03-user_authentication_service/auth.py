441ca4ce38bec774f6f87791cef783419bcf9001
Hassan Ibrahim <hassan2002_kimo@hotmail.com>
2025-06-03 17:48:59 +0000

    0x03 task 8 fix

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -29,7 +29,7 @@ class Auth:
             return self._db.add_user(email, _hash_password(password))
         raise ValueError("User {} already exists".format(email))
 
-    def valid_login(email: str, password: str) -> bool:
+    def valid_login(self, email: str, password: str) -> bool:
         """checks if user credentials are valid"""
         try:
             user = self._db.find_user_by(email=email)
