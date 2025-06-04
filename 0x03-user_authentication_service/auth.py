a45c260560b45ff1a437ab7bb0f2a18ce6074be3
Hassan Ibrahim <hassan2002_kimo@hotmail.com>
2025-06-04 19:25:14 +0000

    0x03 task 16

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -79,10 +79,9 @@ class Auth:
 
     def get_reset_password_token(self, email: str) -> str:
         """generates a reset password token"""
-        try:
-            user = self._dp.find_user_by(email=email)
-        except ValueError:
-            raise
+        user = self._dp.find_user_by(email=email)
+        if user is None:
+            raise ValueError
         reset_token = _generate_uuid()
         self._db.update_user(user.id, reset_token=reset_token)
         return reset_token
